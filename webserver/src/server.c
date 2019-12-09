461381a5b390e1eedda4c197fca41af1d2e0a16f
Jan-Philipp Nuber <jan-philipp.nuber@uni-konstanz.de>
2019-12-09 23:40:01 +0100

    Replace server.c Response fix

--- webserver/src/server.c
+++ webserver/src/server.c
@@ -9,165 +9,167 @@
 #include <unistd.h>
 #include "server_utils.h"
 
-#define DEFAULT_HOST "localhost"
-#define DEFAULT_PORT "8080"
-#define LISTENQUEUE 256 /* This server can only process one client simultaneously * \ \
-                         * How many connections do we want to queue? */
+#define DEFAULT_HOST    "localhost"
+#define DEFAULT_PORT    "8080"
+#define LISTENQUEUE     256 /* This server can only process one client simultaneously * \ \
+	                          * How many connections do we want to queue? */
 
 int startServer(const char *iface, const char *port, struct addrinfo *res)
 {
-    int listenfd;
+	int listenfd;
 
-    struct addrinfo hints;
+	struct addrinfo hints;
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET; /* type of socket */
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
-    hints.ai_flags = AI_PASSIVE; /* socket will use bind later */
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family   = AF_INET;      /* type of socket */
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags    = AI_PASSIVE;      /* socket will use bind later */
 
-    if (iface == NULL)
-    {
-        iface = DEFAULT_HOST;
-    }
+	if (iface == NULL)
+	{
+		iface = DEFAULT_HOST;
+	}
 
-    if (getaddrinfo(iface, port, &hints, &res) != 0)
-    {
-        perror("Could not get address info");
-        exit(1);
-    }
+	if (getaddrinfo(iface, port, &hints, &res) != 0)
+	{
+		perror("Could not get address info");
+		exit(1);
+	}
 
-    listenfd = socket(res->ai_family, res->ai_socktype, 0);
+	listenfd = socket(res->ai_family, res->ai_socktype, 0);
 
-    if (bind(listenfd, res->ai_addr, res->ai_addrlen) != 0)
-    {
-        perror("Could not bind to address");
-        exit(1);
-    }
+	if (bind(listenfd, res->ai_addr, res->ai_addrlen) != 0)
+	{
+		perror("Could not bind to address");
+		exit(1);
+	}
 
-    if (listen(listenfd, LISTENQUEUE) != 0)
-    {
-        perror("Could not listen for connections on socket");
-    }
+	if (listen(listenfd, LISTENQUEUE) != 0)
+	{
+		perror("Could not listen for connections on socket");
+	}
 
-    printf("Server listening on %s:%s...\n", iface, port);
+	printf("Server listening on %s:%s...\n", iface, port);
 
-    return listenfd;
+	return listenfd;
 }
+
 
 void closeServer(struct addrinfo *res)
 {
-    freeaddrinfo(res);
+	freeaddrinfo(res);
 }
+
 
 int main(int argc, char *argv[])
 {
-    const char *iface = NULL;
-    const char *port = argc == 2 ? argv[1] : DEFAULT_PORT;
-    struct addrinfo *res = NULL;
+	const char      *iface = NULL;
+	const char      *port  = argc == 2 ? argv[1] : DEFAULT_PORT;
+	struct addrinfo *res   = NULL;
 
-    int listenfd = startServer(iface, port, res);
+	int listenfd = startServer(iface, port, res);
 
-    char request[MAX_MESSAGE_SIZE];
+	char request[MAX_MESSAGE_SIZE];
 
-    unsigned int request_num = 0;
+	unsigned int request_num = 0;
 
-    while (1)
-    {
-        struct sockaddr_in clientaddr;
-        socklen_t addrlen = sizeof(clientaddr);
-        int client = accept(listenfd, (struct sockaddr *)&clientaddr, &addrlen);
+	while (1)
+	{
+		struct sockaddr_in clientaddr;
+		socklen_t          addrlen = sizeof(clientaddr);
+		int                client  = accept(listenfd, (struct sockaddr *)&clientaddr, &addrlen);
 
-        if (client < 0)
-        {
-            perror("accept error");
-        }
+		if (client < 0)
+		{
+			perror("accept error");
+		}
 
-        ssize_t result = recv(client, request, MAX_MESSAGE_SIZE, 0);
+		ssize_t result = recv(client, request, MAX_MESSAGE_SIZE, 0);
 
-        if (result > 0)
-        {
-            request[result] = '\0';
-            request_num++;
-        }
+		if (result > 0)
+		{
+			request[result] = '\0';
+			request_num++;
+		}
 
-        if (result == 0)
-        {
-            printf("Connection closed\n");
-            break;
-        }
+		if (result == 0)
+		{
+			printf("Connection closed\n");
+			break;
+		}
 
-        if (result < 0)
-        {
-            perror("recv failed");
-            break;
-        }
+		if (result < 0)
+		{
+			perror("recv failed");
+			break;
+		}
 
-        // shutdown(client, SHUT_RD);
+		// shutdown(client, SHUT_RD);
 
-        printf("\n=========================REQUEST %u=====================\n", request_num);
-        printf("%s\n", request);
-        puts("========================================================\n");
+		printf("\n=========================REQUEST %u=====================\n", request_num);
+		printf("%s\n", request);
+		puts("\n========================================================\n");
 
-        Request *request_struct = malloc(sizeof(Request));
+		Request *request_struct = malloc(sizeof(Request));
 
-        parse_request(request_struct, request);
+		parse_request(request_struct, request);
 
-        // int request_res = 0;
+		// int request_res = 0;
 
-        // if (validate_request(request))
-        // {
-        //     parse_request(request_struct, request);
-        //     request_res = request_result(request_struct);
-        // }
-        // else
-        // {
-        //     request_res = 400;
-        // }
+		// if (validate_request(request))
+		// {
+		//     parse_request(request_struct, request);
+		//     request_res = request_result(request_struct);
+		// }
+		// else
+		// {
+		//     request_res = 400;
+		// }
 
-        puts("\nMETHOD:");
-        printf("%s\n", request_struct->method);
+		/*	puts("\nMETHOD:");
+		 *      printf("%s\n", request_struct->method);
+		 *
+		 *      puts("\nPATH:");
+		 *      printf("%s\n", request_struct->path);
+		 *
+		 *      puts("\nCONTENT-LENGTH:");
+		 *      printf("%zu\n", request_struct->content_length);
+		 *
+		 *      puts("\nKEYS:");
+		 *      print_all_keys(request_struct);
+		 *
+		 *      puts("\nVALUES:");
+		 *      print_all_values(request_struct);*/
 
-        puts("\nPATH:");
-        printf("%s\n", request_struct->path);
+		//char *response;
+		//size_t response_len = generate_response_deprecated(&response, request_res, request_struct.path, request_struct.keys, request_struct.values, request_struct.fields_amount);
 
-        puts("\nCONTENT-LENGTH:");
-        printf("%zu\n", request_struct->content_length);
+		Response *response = response_generate(request_struct);
+                char *response_str=malloc(response->content_length + MAX_MESSAGE_SIZE);
+		response_str = response_to_string(response);
 
-        puts("\nKEYS:");
-        request_print_all_keys(request_struct);
+		puts("\n========================RESPONSE========================\n");
+		printf("%s", response_str);
+		puts("========================================================\n");
 
-        puts("\nVALUES:");
-        request_print_all_values(request_struct);
+		ssize_t reply = send(client, response_str, strlen(response_str), 0);
 
-        //char *response;
-        //size_t response_len = generate_response_deprecated(&response, request_res, request_struct.path, request_struct.keys, request_struct.values, request_struct.fields_amount);
+		if (reply == -1)
+		{
+			perror("Send failed");
+		}
+		else
+		{
+			perror("Sended");
+		}
 
-        Response *response = response_generate(request_struct);
+		response_free(response);
 
-        char *response_str = response_to_string(response);
+		close(client);
+	}
 
-        puts("\n========================RESPONSE========================\n");
-        printf("%s", response_str);
-        puts("========================================================\n");
+	closeServer(res);
 
-        ssize_t reply = send(client, response, strlen(response_str), 0);
-
-        if (reply == -1)
-        {
-            perror("Send failed");
-        }
-        else
-        {
-            perror("Sended");
-        }
-
-        response_free(response);
-
-        close(client);
-    }
-
-    closeServer(res);
-
-    return 0;
+	return 0;
 }
