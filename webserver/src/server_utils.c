988cc01956a47f10216ae16e5d45a7e088dfb0de
Jan-Philipp Nuber <jan-philipp.nuber@uni-konstanz.de>
2020-01-07 03:32:40 +0100

    Update server_utils.c

--- webserver/src/server_utils.c
+++ webserver/src/server_utils.c
@@ -370,9 +370,9 @@ Response *response_generate(Request *request, Cache *cache)
                 {
                         exit(1);
                 }
-                entry->path = calloc(sizeof(abspath)+1);
+                entry->path = calloc(1,sizeof(abspath)+1);
                 strcpy(entry->path,abspath);
-                entry->data = calloc(sizeof(file_bytes)+1);
+                entry->data = calloc(1,sizeof(file_bytes)+1);
                 strcpy(entry->data, file_bytes);
                 entry->lastacc=10;
                 ssize_t insert = insertEntry(cache, entry);
